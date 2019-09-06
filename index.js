8dde26d5070c8ab7cae726107b372af1eca0e72f
LPascholatti <lucaspascholatti@gmail.com>
2019-09-06 11:45:04 +0200

    created an image model and database with Sequelize

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 const db = require('./db')
+const model = require('./images/model')
 
 app.get('/', (req, res) => res.send('This API is working'))
 
