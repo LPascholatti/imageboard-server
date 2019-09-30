158b5f20a4b2498ca9b2d01dd3dc6cf22243b672
LPascholatti <lucaspascholatti@gmail.com>
2019-09-30 12:03:44 +0200

    commiting a post request in the the image router

--- images/router.js
+++ images/router.js
@@ -3,10 +3,17 @@ const Image = require('./model')
 const router = new Router()
 
 router.get('/image', (req, res, next) =>
-Image
-.findAll()
-.then(displayImage => res.json(displayImage))
-.catch(next)
+  Image
+    .findAll()
+    .then(displayImage => res.json(displayImage))
+    .catch(next)
+)
+
+router.post('/image', (req, res, next) =>
+  Image
+    .create(req.body)
+    .then(newImage => res.json(newImage))
+    .catch(next)
 )
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
-const db = require('./db')
-const model = require('./images/model')
+//const db = require('./db')
+//const model = require('./images/model')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const imageRouter = require('./images/router')
 const authRouter = require('./auth/router')
+const userRouter = require('./user/router')
+const middleware = cors()
 
+app.use(middleware)
 app.use(jsonParser)
 app.use(imageRouter)
 app.use(authRouter)
+app.use(userRouter)
 
 app.get('/', (req, res) => res.send('This API is working'))
 
-app.listen(port, () => console.log(`Example App listening on port ${port}`))
+function onListen() {
+  console.log(`Example App listening on port ${port}`)
+}
+
+app.listen(port, onListen)
 
